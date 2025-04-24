3b2cc264cf479b477744f5e3b99d3e5969e4442e
Abbas El <ab-el11@hotmail.com>
2025-04-24 20:26:55 +0200

    added aboutme and skills component-finished the aboutme section /the text must be changed- added the skills content and added the hover effect on the icons - added and finished the pull to peel animation

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from './hero/hero.component';
-
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @Component({
   selector: 'app-main',
-  imports: [HeroComponent],
+  imports: [HeroComponent,AboutmeComponent,SkillsComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
